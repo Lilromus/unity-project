e21556042734a9e45bfb3a5ea80edb74764a7c0a
Lilromus <romariokruk@gmail.com>
2023-10-19 20:39:03 +0200

    ruch i przewracanie
    ruch na strzalkach lewo i prawy + postac sie przewraca do tylu jezeli gracz biegnie do tylu

--- Our project/Assets/PlayerMovement.cs
+++ Our project/Assets/PlayerMovement.cs
@@ -1,22 +1,58 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+
+    private bool isGrounded;
+    public Transform feetPos;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    private bool isJumping;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
+
+    void FixedUpdate()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown("space"))
+
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius);
+        if(moveInput > 0)
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0,10, 0);
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else if(moveInput < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        if (Input.GetKeyDown("space") && isGrounded == true && !isJumping)
+        {
+            StartCoroutine(JumpWithDelay());
         }
     }
 
-
+    IEnumerator JumpWithDelay()
+    {
+        isJumping = true;
+        rb.velocity = Vector2.up * jumpForce;
+        yield return new WaitForSeconds(0.78f); // Adjust the delay time as needed
+        isJumping = false;
+        isGrounded = true;
+    }
 }
+
