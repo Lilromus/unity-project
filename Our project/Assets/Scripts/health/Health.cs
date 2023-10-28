0641df86e83533d95bccc5499a75a74a400ebe72
Lilromus <romariokruk@gmail.com>
2023-10-28 16:11:54 +0200

    fsg

--- Our project/Assets/Scripts/health/Health.cs
+++ Our project/Assets/Scripts/health/Health.cs
@@ -1,35 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; }
-
+    private Animator anim;
+    private bool dead;
 
     private void Awake()
     {
         currentHealth = startingHealth;
+        anim = GetComponent<Animator>();
     }
     public void TakeDamage(float _damage)
     {
         currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
 
-        if(currentHealth > 0)
+        if (currentHealth > 0)
         {
-            //player hurt
+            anim.SetTrigger("hurt");
+            //iframes
         }
         else
         {
-            //whaterver
+            if (!dead)
+            {
+                anim.SetTrigger("die");
+                GetComponent<PlayerMovement>().enabled = false;
+                dead = true;
+            }
         }
     }
-    private void Update()
+    public void AddHealth(float _value)
     {
-        if(Input.GetKeyDown(KeyCode.E))
-        {
-            TakeDamage(1);
-        }
+        currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
     }
 }
