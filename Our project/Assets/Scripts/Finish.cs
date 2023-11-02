d08e4661932bdb51e0f92c08e0e97d803fdb31a6
Lilromus <romariokruk@gmail.com>
2023-11-02 15:54:44 +0200

    tlo i przejscie miedzy lvlami

--- Our project/Assets/Scripts/Finish.cs
+++ Our project/Assets/Scripts/Finish.cs
@@ -1,25 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour
 {
     [SerializeField] private AudioSource finishSound;
     // Update is called once per frame
+    private bool levelCompleted = false;
     private void Start()
     {
         finishSound = GetComponent<AudioSource>();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.name=="Player")
+        if (collision.gameObject.name=="Player" && !levelCompleted)
         {
             finishSound.Play();
-            CompleteLevel();
+            levelCompleted = true;
+            Invoke("CompleteLevel", 2f);
         }
     }
     private void CompleteLevel()
     {
-
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
